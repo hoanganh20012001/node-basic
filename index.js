f8e81dad83f3c48b6ffb1eff15cbd441f6625873
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2021-08-14 21:42:42 +0700

    params & query

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const port = 3000
 
 app.use(morgan('combined'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/v1/users/:id', (req, res) => {
+  // console.log(req.params)
+  // console.log(req.query)
+  res.json([
+    {
+      id: 1,
+      name: 'John',
+    }
+  ])
 })
 
 app.listen(port, () => {
